1778052c46a434f62664b677e6e28dc8541880eb
bealabea <beatricemarin89@yahoo.it>
2021-12-14 15:27:02 +0100

    Js instructions for News Carousel

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Creo un array di titoli
+
+// Creo un array di paragrafi
+
+// creo una variabile currentIndex con valore 0
+
+// creo le variabili per titoli, paragrafi e button
+
+// attribuisco il correntIndex alle variabili di titolo e paragrafo
+
+// creo un evento al click dei button:
+// buttonAfter con currentIndex++ (per incrementare il numero e scorrere in avanti la lista) a cui indico che SE supera la fine deve ricominciare dall'inizio della lista;
+// buttonBefore con currentIndex-- (per decrementare il numero e scorrere indietro la lista) a cui indico che SE supera lo "zero" deve ricominciare dalla fine della lista.
